104ae259462bde2a08195943b2448f5c626ae041
alfre <alfrednilausen@gmail.com>
2026-06-17 13:16:06 +0200

    næsten

--- fourcorners.py
+++ fourcorners.py
@@ -29,17 +29,17 @@ DISPLAY_SCALE = 0.5
 MARKER_OFFSETS_MM = {
     "FL": (0,  -85),
     "FR": ( 0,  85),
-    "BL": (-490, -85),
-    "BR": ( -490, 85),
+    "BL": (-370, -85),
+    "BR": ( -370, 85),
 }
 
 # green detection
 GREEN_H_MIN = 25
 GREEN_H_MAX = 95
-GREEN_S_MIN = 50
-GREEN_V_MIN = 50
+GREEN_S_MIN = 40
+GREEN_V_MIN = 40
 
-MIN_BLOB_AREA = 60
+MIN_BLOB_AREA = 10
 
 ROBOT_MARKER_ID = 0
 ARUCO_DICT = cv2.aruco.DICT_4X4_50
